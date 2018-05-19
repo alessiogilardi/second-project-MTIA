4f80e13716511a37a6ac65674694ebc4adf8ecdf
alessiogilardi <36900296+alessiogilardi@users.noreply.github.com>
2018-05-19 21:11:32 +0200

    edit

--- primo.m
+++ primo.m
@@ -10,9 +10,9 @@ a = [4 5 3 2; 2 10 1 4]; % Temp1 di elaborazione in ciascuna stazione
 %t = ones(assembly_lines, n); 
 t = [0 7 4 5 ; 0 9 2 8]; % Tempi di cambio di linea
 
-e = [10 12]; % Tempi di entrata
+e = [10 12]'; % Tempi di entrata
 
-x = [18 7]; % Tempi di uscita dalla fabbrica
+x = [18 7]'; % Tempi di uscita dalla fabbrica
 
 leave_time = zeros(assembly_lines, n);
 
@@ -28,7 +28,10 @@ for j = 2:n
     leave_time(2,j) = min(leave_time(2,j-1)+a(2,j), leave_time(1,j-1)+t(1,j)+a(2,j));
 end
 
-min_time = min(leave_time(1,n)+x(1), leave_time(2,n)+x(2));
+%min_time = min(leave_time(1,n)+x(1), leave_time(2,n)+x(2));
+
+[min_time, ind] = min(leave_time(:,n) + x);
+
 
 
 
