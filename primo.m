0b3c18e06cd9c61a2a0d9590864f2827fe78f484
alessiogilardi <36900296+alessiogilardi@users.noreply.github.com>
2018-05-22 19:06:18 +0200

    raw solution

--- primo.m
+++ primo.m
@@ -21,15 +21,34 @@ for i = 1:assembly_lines
     leave_time(i,1) = e(i) + a(i,1);
 end
 
+
+prec = zeros(assembly_lines, n);
+pass = zeros(assembly_lines, n); % Vettore di passaggio
 % Calcolo
 
 for j = 2:n
-    leave_time(1,j) = min(leave_time(1,j-1)+a(1,j), leave_time(2,j-1)+t(2,j)+a(1,j));
-    leave_time(2,j) = min(leave_time(2,j-1)+a(2,j), leave_time(1,j-1)+t(1,j)+a(2,j));
+    [leave_time(1,j), prec(1,j)] = min([leave_time(1,j-1)+a(1,j), leave_time(2,j-1)+t(2,j)+a(1,j)]);
+    %[leave_time(2,j), prec(2,j)] = min([leave_time(2,j-1)+a(2,j), leave_time(1,j-1)+t(1,j)+a(2,j)]);
+    [leave_time(2,j), prec(2,j)] = min([leave_time(1,j-1)+t(1,j)+a(2,j), leave_time(2,j-1)+a(2,j)]);
 end
 
 
 [min_time, ind] = min(leave_time(:,n) + x);
+
+pass(ind, n) = 1;
+line = ind;
+for j = n-1:-1:1
+    [m,i] = min([leave_time(not_line(line),j)+t(not_line(line),j+1)+a(line,j+1), leave_time(line,j)+a(line,j+1)]);
+    if i == 1
+        % vengo dall'altra linea
+        line = not_line(line);
+        pass(line,j) = 1;
+    else
+        %vengo dalla stessa linea
+        pass(line,j) = 1;
+    end
+    %if leave_time(not_line,j)+t(not_line,j+1)+a(line,j+1) 
+end
 
 
 
