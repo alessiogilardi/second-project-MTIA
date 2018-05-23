914e237459e0716ce4fba944b3502126f761e3e9
alessiogilardi <36900296+alessiogilardi@users.noreply.github.com>
2018-05-23 09:00:55 +0200

    piccole correzioni e pulizia(codice da provare)

--- primo.m
+++ primo.m
@@ -5,10 +5,10 @@ assembly_lines = 2; % Numero di linee di assemblaggio
 n = 4; % Numero di stazioni di assemblaggio per ogni linea
 
 %a = ones(assembly_lines, n);
-a = [4 5 3 2; 2 10 1 4]; % Temp1 di elaborazione in ciascuna stazione
+a = [4 5 3 2; 2 10 1 4]; % Tempi di elaborazione in ciascuna stazione
 
 %t = ones(assembly_lines, n); 
-t = [0 7 4 5 ; 0 9 2 8]; % Tempi di cambio di linea
+t = [0 7 4 5; 0 9 2 8]; % Tempi di cambio di linea
 
 e = [10 12]'; % Tempi di entrata
 
@@ -17,35 +17,43 @@ x = [18 7]'; % Tempi di uscita dalla fabbrica
 leave_time = zeros(assembly_lines, n);
 
 % Inizializzazione
-for i = 1:assembly_lines
-    leave_time(i,1) = e(i) + a(i,1);
-end
+%for i = 1:assembly_lines
+    %leave_time(i,1) = e(i) + a(i,1);
+%end
+% Da sostituire con
+leave_time(:,1) = e(:) + a(:,1);
 
+pass = zeros(assembly_lines, n); % Vettore di passaggio -> indica il passaggio o meno da una stazione
 
-pass = zeros(assembly_lines, n); % Vettore di passaggio
-% Calcolo
-
+% Calcolo dei leave time per ogni stazione
 for j = 2:n
     leave_time(1,j) = min(leave_time(1,j-1)+a(1,j), leave_time(2,j-1)+t(2,j)+a(1,j));
     leave_time(2,j) = min(leave_time(2,j-1)+a(2,j), leave_time(1,j-1)+t(1,j)+a(2,j));
 end
 
 
-[min_time, ind] = min(leave_time(:,n) + x);
+[min_time, line] = min(leave_time(:,n) + x);
 
-pass(ind, n) = 1;
-line = ind;
+pass(line, n) = 1;
 for j = n-1:-1:1
     [m,i] = min([leave_time(not_line(line),j)+t(not_line(line),j+1)+a(line,j+1), leave_time(line,j)+a(line,j+1)]);
-    if i == 1
+    %if i == 1
         % vengo dall'altra linea
+        %line = not_line(line);
+        %pass(line,j) = 1;
+    %else
+        % vengo dalla stessa linea
+        %pass(line,j) = 1;
+    %end
+    
+    % Codice da provare %
+    if i == 1
+        % Vengo dall'altra linea
+        % quindi faccio un cambio di linea
         line = not_line(line);
-        pass(line,j) = 1;
-    else
-        %vengo dalla stessa linea
-        pass(line,j) = 1;
     end
-    %if leave_time(not_line,j)+t(not_line,j+1)+a(line,j+1) 
+    pass(line,j) = 1;
+    %*******************%
 end
 
 
