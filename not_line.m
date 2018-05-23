f4c7d44c2e1c0332e47ceb1fb973894e05c6e89a
alessiogilardi <36900296+alessiogilardi@users.noreply.github.com>
2018-05-23 09:03:24 +0200

    code cleaned

--- not_line.m
+++ not_line.m
@@ -1,7 +1,7 @@
 % La funzione restituisce la linea di assemblaggio opposta a quella attuale
 function not_line = not_line(line)
+not_line = 1;
 if line == 1
     not_line = 2;
-else
-    not_line = 1;
+end
 end
