1a2bc23ca92b030e8f0632c0f6a9700e2ce7899c
alessiogilardi <36900296+alessiogilardi@users.noreply.github.com>
2018-06-14 18:06:06 +0200

    edit

--- brute.m
+++ brute.m
@@ -4,7 +4,7 @@ clear;
 % Dati del problema
 
 lines = 2; % Numero di linee di assemblaggio
-n = 3; % Numero di stazioni di assemblaggio per ogni linea
+N = 4; % Numero di stazioni di assemblaggio per ogni linea
 
 %a = ones(assembly_lines, n);
 a = [4 5 3 2; 2 10 1 4]; % Tempi di elaborazione in ciascuna stazione
@@ -16,21 +16,38 @@ e = [10 12]'; % Tempi di entrata
 
 x = [18 7]'; % Tempi di uscita dalla fabbrica
 
-num_paths = 2^n;
-S = zeros(lines, n, num_paths);
+num_paths = 2^N;
+S = zeros(lines, N, num_paths);
 
 i=1;
-j=1;
-%for j=1:n
-    for k = 1:num_paths/2
-        S(i,j,k) = 1;
+count=0;
+for j=1:N
+    k=0;
+    for p=1:(2^j)
+        for c=1:(num_paths/(2^j))
+            k=k+1;
+            S(i,j,k) = 1;
+            count=count+1;
+        end
         i = not_line(i);
     end
-%end  
+end
+
+% Verifico che non ci siano percorsi ripetuti
+
+% o=-1;
+% for c=1:num_paths
+%     for d=1:num_paths
+%         if (not(c==d) && isequal(S(:,:,c),S(:,:,d)))
+%             o
+%         end
+%     end
+% end
+
+for c=1:num_paths
     
-    
-    
-    
-    
-    
-    
+end
+
+
+
+
