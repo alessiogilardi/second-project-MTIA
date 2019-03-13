78b168b22ea1919e2e02e2ae1246d49d2441fc84
kartista88 <mauro91ge@gmail.com>
2019-03-13 14:15:13 +0100

    up

--- solutions/DP_solution.m
+++ solutions/DP_solution.m
@@ -12,7 +12,7 @@ leave_time = zeros(lines, N);
 %   stazioni delle due linee
 leave_time(:,1) = e(:) + a(:,1);
 
-%%  Algoritmo DP del costo minimo
+%%  Algoritmo DP del costo minimo (fase forward)
 %   Calcolo dei leave time per ogni stazione all'interno della catena
 for j = 2:N
     leave_time(1,j) = min(leave_time(1,j-1)+a(1,j), leave_time(2,j-1)+t(2,j)+a(1,j));
@@ -25,11 +25,12 @@ end
 %   la linea da cui sono uscito 
 [min_time, line] = min(leave_time(:,N) + x);
 
-%%  Calcolo percorso a costo minimo
+%%  Calcolo percorso a costo minimo (fase backward)
 %   Matrice di passaggio min_path -> indica il passaggio o meno da una stazione
 min_path = zeros(lines, N); 
 min_path(line, N) = 1;
 for j = N-1:-1:1
+    % Capisco se  ho cambiato linea o arrivo dalla stessa linea
     [m,i] = min([leave_time(not_line(line),j)+t(not_line(line),j+1)+a(line,j+1),    leave_time(line,j)+a(line,j+1)]);    
     if i == 1
         %   Vengo dall'altra linea quindi faccio un cambio di linea
